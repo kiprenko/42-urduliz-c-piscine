17617b57b89ae8d4f937e7f84e994663ea102a1d
Yevhenii Kiprenko <ykiprenk@c1r5s4.42urduliz.com>
2024-06-10 23:25:12 +0200

    Implemented ex05

---
+++ c01/ex05/ft_pustr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pustr.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 23:17:01 by ykiprenk          #+#    #+#             */
+/*   Updated: 2024/06/10 23:24:09 by ykiprenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+/*
+int	main(void)
+{
+	char	*str;
+
+	str = "Esta es la linea para imprimir letra a letra";
+	ft_putstr(str);
+	return (0);
+}*/
