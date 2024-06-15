e3ab539f11406f4c9b6173b9b91176d8b97cede5
Yevhenii Kiprenko <ykiprenk@c1r7s2.42urduliz.com>
2024-06-15 21:07:47 +0200

    Fixed ex10

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 20:20:16 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/15 20:52:31 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/15 21:07:31 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -29,18 +29,16 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	int				srclen;
 
 	srclen = ft_strlen(src);
-	if (srclen == 0)
+	if (srclen > 0)
 	{
-		dest[0] = '\0';
-		return (srclen);
+		i = 0;
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	i = 0;
-	while (src[i] != '\n' && i < size)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[size] = '\n';
 	return (srclen);
 }
 /*
@@ -49,10 +47,10 @@ int	main(void)
 	char src[] = "Nobody knows I use ChatGPT";
 	char dest[] = "JUST A PLACE HOLDER JUST A PLACE HOLDER";
 
-	ft_strlcpy(dest, src, 26);
+	ft_strlcpy(dest, src, 27);
 	int i = 0;
 	char c;
-	while (dest[i] != '\n')
+	while (dest[i] != '\0')
 	{
 		c = dest[i];
 		write(1, &c, 1);
