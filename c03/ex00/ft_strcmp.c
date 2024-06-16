87c4b210de8cb03e90435bb5107f18a06ce82594
Yevhenii Kiprenko <ykiprenk@c1r6s4.42urduliz.com>
2024-06-16 14:04:27 +0200

    Fixed ex00

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 13:40:48 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/16 13:56:40 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/16 14:04:16 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -14,23 +14,19 @@
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
-	
+
 	i = 0;
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] == s2[i])
 	{
-		if (s1[i] > s2[i])
+		if (s1[i] == '\0')
 		{
-			return (1);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-1);
+			return (0);
 		}
 		i++;
-	}	
-	return (0);
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
-
+/*
 int	main(void)
 {
 	char	s1[] = "Hello, world!";
@@ -49,4 +45,4 @@ int	main(void)
 		write(1, "s1 eq s2", 8);
 	}
 	return (0);
-}
+}*/
