16076fb17b2d340be2d505d82871777b5e755239
Yevhenii Kiprenko <ykiprenk@c1r6s4.42urduliz.com>
2024-06-16 18:35:53 +0200

    Fixed compilation of c02/ex01

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 23:14:51 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/14 00:12:48 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/16 18:35:29 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (src[i] != '\0' && i < n)
@@ -31,27 +31,8 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 /*
 int main() {
     char dest[20];
-
-    // Test case 1
     ft_strncpy(dest, "Hello, world!", 5);
-    printf("Test 1: \"%s\"\n", dest);
-
-    // Test case 2
-    ft_strncpy(dest, "Hello, world!", 20);
-    printf("Test 2: \"%s\"\n", dest);
-
-    // Test case 3
-    ft_strncpy(dest, "Hello", 10);
-    printf("Test 3: \"%s\"\n", dest);
-
-    // Test case 4
-    ft_strncpy(dest, "", 5);
-    printf("Test 4: \"%s\"\n", dest);
-
-    // Test case 5
-    ft_strncpy(dest, "A longer string to test", 10);
-    printf("Test 5: \"%s\"\n", dest);
-
+    printf("Test: \"%s\"\n", dest);
     return 0;
 }
 */
