6f2ec03f8abc1b07b15b0399a58a3c6ab3b95ae0
Yevhenii Kiprenko <ykiprenk@c1r7s2.42urduliz.com>
2024-06-17 20:46:10 +0200

    Fixed ex01

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 13:40:48 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/16 14:13:13 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/17 20:45:38 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -15,14 +15,18 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
-	i = 0;
-	while (s1[i] == s2[i] && i < n - 1)
+	if (n == 0)
 	{
-		if (s1[i] == '\0')
-		{
-			return (0);
-		}
+		return (0);
+	}
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n)
+	{
 		i++;
+	}
+	if (i == n)
+	{
+		return (0);
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
@@ -30,7 +34,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 int	main(void)
 {
 	char	s1[] = "Hello, world!";
-	char	s2[] = "Hello, a world!";
+	char	s2[] = "Hellz, a world!";
 	int	cmpres = ft_strncmp(s1, s2, 5);
 	if (cmpres > 0)
 	{
