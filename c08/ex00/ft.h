5905be52adf20ea0addd89046dde6f080d7e9605
Yevhenii Kiprenko <ykiprenk@c1r8s6.42urduliz.com>
2024-06-19 17:37:06 +0200

    Update the fie according to norminette

--- c08/ex00/ft.h
+++ c08/ex00/ft.h
@@ -6,12 +6,16 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 17:29:20 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/19 17:31:02 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/19 17:36:38 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef FT_H
+# define FT_H
 
 void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
+
+#endif
