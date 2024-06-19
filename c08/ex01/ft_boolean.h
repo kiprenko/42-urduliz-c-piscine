96b520ad5745fb4cecd78ebf2a7600d272e5928c
Yevhenii Kiprenko <ykiprenk@c1r6s4.42urduliz.com>
2024-06-19 22:00:34 +0200

    Fixes for ex01 norminette

--- c08/ex01/ft_boolean.h
+++ c08/ex01/ft_boolean.h
@@ -6,17 +6,26 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 17:41:00 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/19 20:40:57 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/19 21:33:50 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#ifndef T_BOOL_H
+# define T_BOOL_H
 
-# define t_bool int
-# define TRUE 1
-# define FALSE 0
+# include <unistd.h>
+
+# ifndef TRUE
+#  define TRUE 1
+# endif
+
+# ifndef FALSE
+#  define FALSE 0
+# endif
+
+typedef int	t_bool;
 # define EVEN_MSG "I have an even number of arguments." 
 # define ODD_MSG "I have an odd number of arguments."
 # define SUCCESS 0
-# define EVEN(n) (n % 2 == 0)
-
+# define EVEN(n) ((n) % 2 == 0)
+#endif
