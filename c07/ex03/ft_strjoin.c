5fe220a3688339c08d427037eeeb49792bcedae7
Yevhenii Kiprenko <ykiprenk@c1r7s6.42urduliz.com>
2024-06-19 10:31:04 +0200

    Updated ex03 according to norminette

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -6,12 +6,13 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 23:30:57 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/19 10:21:53 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/19 10:30:44 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
+#include <stdio.h>
 
-unsigned int	ft_strlen(char * str)
+unsigned int	ft_strlen(char *str)
 {
 	int	i;
 
@@ -20,19 +21,27 @@ unsigned int	ft_strlen(char * str)
 	{
 		i++;
 	}
-	return i;
+	return (i);
 }
 
-unsigned int	ft_count_cat_len()
+unsigned int	ft_count_cat_len(int size, char **strs, char *sep)
 {
+	int	i;
+	int	catlen;
 
+	catlen = (size - 1) * ft_strlen(sep) + 1;
+	i = 0;
+	while (i < size)
+	{
+		catlen += ft_strlen(strs[i]);
+		i++;
+	}
+	return (catlen);
 }
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
 	char	*cat;
-	int		catlen;
-	int		seplen;
 	int		i;
 	int		j;
 	int		k;
@@ -44,15 +53,7 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 		cat[0] = '\0';
 		return (cat);
 	}
-	seplen = ft_strlen(sep);
-	catlen = (size - 1) * seplen + 1;
-	i = 0;
-	while(i < size)
-	{
-		catlen += ft_strlen(strs[i]);
-		i++;
-	}
-	cat = (char *) malloc(catlen * sizeof(char));
+	cat = (char *) malloc(ft_count_cat_len(size, strs, sep) * sizeof(char));
 	if (cat == NULL)
 		return (NULL);
 	i = 0;
@@ -60,7 +61,7 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	while (i < size)
 	{
 		j = 0;
-		while(strs[i][j] != '\0')
+		while (strs[i][j] != '\0')
 		{
 			cat[k] = strs[i][j];
 			j++;
@@ -69,7 +70,7 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 		if (i + 1 < size)
 		{
 			m = 0;
-			while(sep[m] != '\0')
+			while (sep[m] != '\0')
 			{
 				cat[k] = sep[m];
 				m++;
@@ -81,5 +82,49 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	cat[k] = '\0';
 	return (cat);
 }
+/*
+int main() {
+    // Test Case 1: size = 0, should return an empty string
+    char *empty[] = {};
+    char *result = ft_strjoin(0, empty, ",");
+    printf("Test Case 1: \"%s\"\n", result);
+    free(result);
 
+    // Test Case 2: size = 1, single string, 
+    // should return the single string without separator
+    char *single[] = {"Hello"};
+    result = ft_strjoin(1, single, ",");
+    printf("Test Case 2: \"%s\"\n", result);
+    free(result);
 
+    // Test Case 3: size = 3, multiple strings, 
+    // should join strings with separator
+    char *multiple[] = {"Hello", "world", "test"};
+    result = ft_strjoin(3, multiple, ", ");
+    printf("Test Case 3: \"%s\"\n", result);
+    free(result);
+
+    // Test Case 4: size = 2, different separator
+    char *two[] = {"Hello", "world"};
+    result = ft_strjoin(2, two, " + ");
+    printf("Test Case 4: \"%s\"\n", result);
+    free(result);
+
+    // Test Case 5: size = 0, separator non-empty but no strings to join
+    result = ft_strjoin(0, empty, " - ");
+    printf("Test Case 5: \"%s\"\n", result);
+    free(result);
+
+    // Test Case 6: size = 3, empty separator
+    char *three[] = {"This", "is", "test"};
+    result = ft_strjoin(3, three, "");
+    printf("Test Case 6: \"%s\"\n", result);
+    free(result);
+
+    // Test Case 7: size = 3, separator is a space
+    result = ft_strjoin(3, three, " ");
+    printf("Test Case 7: \"%s\"\n", result);
+    free(result);
+
+    return 0;
+}*/
