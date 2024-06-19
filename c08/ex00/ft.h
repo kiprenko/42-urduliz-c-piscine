4508b135b541b43bb98510f1d8f2e31a261e06b4
Yevhenii Kiprenko <ykiprenk@c1r8s6.42urduliz.com>
2024-06-19 17:31:23 +0200

    Corrected according to norminette

--- c08/ex00/ft.h
+++ c08/ex00/ft.h
@@ -6,12 +6,12 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 17:29:20 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/19 17:29:48 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/19 17:31:02 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
-int	ft_strlen(char *str);
-int	ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
