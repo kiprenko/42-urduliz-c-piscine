207652d4e38743a247b559178747fb0adeef48b4
Yevhenii Kiprenko <ykiprenk@c1r6s4.42urduliz.com>
2024-06-19 20:43:10 +0200

    Made working header file for ex01

--- c08/ex01/ft_boolean.h
+++ c08/ex01/ft_boolean.h
@@ -6,8 +6,17 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 17:41:00 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/19 17:41:19 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/19 20:40:57 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+# define t_bool int
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments." 
+# define ODD_MSG "I have an odd number of arguments."
+# define SUCCESS 0
+# define EVEN(n) (n % 2 == 0)
 
