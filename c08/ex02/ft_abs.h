b10abeb561ca174b0dda9d990e0afe1ecfdda337
Yevhenii Kiprenko <ykiprenk@c1r6s6.42urduliz.com>
2024-06-20 23:59:51 +0200

    Implementing ex02

--- c08/ex02/ft_abs.h
+++ c08/ex02/ft_abs.h
@@ -6,8 +6,13 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 23:23:16 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/20 23:23:25 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/20 23:59:16 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#ifndef FT_ABS_H
+# define FT_ABS_H
+# define ABS_CONDITION (condition, true_val, false_val) \
+       	_Generic((0, condition), int: true_val, default: false_val)
+# define ABS(Value) ABS_CONDITION(((Value) < 0), ((Value) * (-1)) : (Value))
+#endif
