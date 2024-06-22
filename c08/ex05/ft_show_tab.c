a439d56dd4687e1347c3fb3a80a387243f65a0da
Yevhenii Kiprenko <ykiprenk@c1r6s6.42urduliz.com>
2024-06-22 17:04:57 +0200

    c08-ex5 done

--- c08/ex05/ft_show_tab.c
+++ c08/ex05/ft_show_tab.c
@@ -6,84 +6,23 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 00:47:25 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/22 16:22:16 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/22 17:04:37 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdlib.h>
-// ex05
+
 #include "ft_stock_str.h"
-#include <stdio.h>
 #include <unistd.h>
 
-unsigned int	ft_strlen(char *str)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-unsigned int	ft_to_s_stock_str(char *str, struct s_stock_str *s)
-{
-	int	i;
-	int	strlen;
-
-	i = 0;
-	strlen = ft_strlen(str);
-	s->size = strlen;
-	s->str = str;
-	s->copy = malloc(strlen * sizeof(char));
-	if (s->copy == NULL)
-		return (0);
-	while (i < strlen)
-	{
-		s->copy[i] = str[i];
-		i++;
-	}
-	return (1);
-}
-
-void	ft_term_arr(struct s_stock_str *s)
-{
-	s->str = 0;
-	s->copy = 0;
-	s->size = 0;
-}
-
-struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
-{
-	struct s_stock_str	*s;
-	int					i;
-
-	s = malloc((ac + 1) * sizeof(struct s_stock_str));
-	if (s == NULL)
-		return (NULL);
-	i = 0;
-	while (i < ac)
-	{
-		if (ft_to_s_stock_str(av[i], &(s[i])))
-			return (NULL);
-		i++;
-	}
-	ft_term_arr(&(s[ac]));
-	return (s);
-}
-
-// ex05
 void	ft_write_str(char *str)
 {
-	while(*str != '\0')
+	while (*str != '\0')
 	{
 		write(1, str, 1);
 		str++;
 	}
 }
 
-void	ft_write_nl()
+void	ft_write_nl(void)
 {
 	write(1, "\n", 1);
 }
@@ -124,18 +63,6 @@ void	ft_show_tab(struct s_stock_str *par)
 		ft_write_nl();
 		ft_write_str(par[i].copy);
 		ft_write_nl();
-		i++;	
+		i++;
 	}
-}
-
-int	main(void)
-{
-	
-	printf("Initializing array of strs");
-	char	*arr[] = {"String1", "String2", "String3"};
-	printf("Converting strs to struct");
-	struct s_stock_str *s = ft_strs_to_tab(3, arr);
-	printf("Converted strs to struct");
-	ft_show_tab(s);
-	return (0);
 }
