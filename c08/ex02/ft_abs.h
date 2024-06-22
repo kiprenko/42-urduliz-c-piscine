8aad2f0fe22e4e2e59093d308f03e6d8ac3f7638
Yevhenii Kiprenko <ykiprenk@c1r6s6.42urduliz.com>
2024-06-22 14:35:41 +0200

    Updated ex02

--- c08/ex02/ft_abs.h
+++ c08/ex02/ft_abs.h
@@ -6,13 +6,12 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 23:23:16 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/21 00:30:04 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/22 14:35:05 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-# define ABS(Value) (((Value) + ((Value) >> (sizeof(Value) * 8 - 1))) \
-		^ (((Value) >> (sizeof(Value) * 8 - 1))))
+# define ABS(Value) ((Value) * (1 - 2 *((Value < 0))))
 
 #endif
