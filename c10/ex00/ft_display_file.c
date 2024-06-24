82edce1b625021f3655407adc705676f6443c8ba
Yevhenii Kiprenko <ykiprenk@c1r5s6.42urduliz.com>
2024-06-24 23:00:20 +0200

    c10-ex00 implemented c part of the ex

--- c10/ex00/ft_display_file.c
+++ c10/ex00/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: ykiprenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 20:57:46 by ykiprenk          #+#    #+#             */
-/*   Updated: 2024/06/24 21:32:54 by ykiprenk         ###   ########.fr       */
+/*   Updated: 2024/06/24 22:59:45 by ykiprenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -14,12 +14,23 @@
 
 #define BUFFER_SIZE 1024
 
+void	ft_write(char buffer[])
+{
+	unsigned int	i;
+
+	i = 0;
+	while (buffer[i] != '\0')
+	{
+		write(1, &buffer[i], 1);
+		i++;
+	}
+}
+
 void	ft_read_file(char *file_name)
 {
-	int	fdef;
-	ssize_t bytes_read;
-	char buffer[BUFFER_SIZE];
-	int	i;
+	int		fdef;
+	ssize_t	bytes_read;
+	char	buffer[BUFFER_SIZE];
 
 	fdef = open(file_name, O_RDONLY);
 	if (fdef == -1)
@@ -27,24 +38,19 @@ void	ft_read_file(char *file_name)
 		write(1, "Cannot read file.\n", 19);
 		return ;
 	}
-	
-	while ((bytes_read = read(fdef, buffer, BUFFER_SIZE - 1)))
+	bytes_read = read(fdef, buffer, BUFFER_SIZE - 1);
+	while (bytes_read > 0)
 	{
 		buffer[bytes_read] = '\0';
-		i = 0;
-		while (buffer[i] != '\0')
-		{
-			write(1, &buffer[i], 1);
-			i++;
-		}
+		ft_write(buffer);
+		bytes_read = read(fdef, buffer, BUFFER_SIZE - 1);
 	}
 	if (bytes_read == -1)
 	{
 		write(1, "Cannot read file.\n", 19);
 		close(fdef);
-		return;
+		return ;
 	}
-
 	if (close(fdef) == -1)
 		write(1, "Cannot read file.\n", 19);
 }
@@ -62,6 +68,5 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	ft_read_file(argv[1]);
-	
 	return (0);
 }
